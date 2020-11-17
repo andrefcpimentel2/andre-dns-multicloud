8dd8e09e98ea68ab6e9ea117af64ed58d18f0c00
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-11-17 12:58:37 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,29 +16,29 @@ module "dns-multicloud" {
 }
 
 output "aws_sub_zone_id" {
-  value = module.dns-multicloud.aws_sub_zone_id[0]
+  value = var.create_aws_dns_zone ? aws_route53_zone.aws_sub_zone[0].zone_id : ""
 }
 
 output "aws_sub_zone_nameservers" {
-  value = module.dns-multicloud.aws_sub_zone_nameservers[0]
+  value = var.create_aws_dns_zone ? aws_route53_zone.aws_sub_zone[0].name_servers : []
 }
 
 output "azure_sub_zone_name" {
-  value = module.dns-multicloud.azure_sub_zone_name[0]
+  value = var.create_azure_dns_zone ? azurerm_dns_zone.azure_sub_zone[0].id : ""
 }
 
 output "azure_sub_zone_nameservers" {
-  value = module.dns-multicloud.azure_sub_zone_nameservers[0]
+  value = var.create_azure_dns_zone ? azurerm_dns_zone.azure_sub_zone[0].name_servers : []
 }
 
 output "azure_dns_resourcegroup" {
-  value = module.dns-multicloud.azure_dns_resourcegroup[0]
+  value = var.create_azure_dns_zone ? azurerm_resource_group.dns_resource_group[0].name : ""
 }
 
 output "gcp_dns_zone_name" {
-  value = module.dns-multicloud.gcp_dns_zone_name[0]
+  value = var.create_gcp_dns_zone ? google_dns_managed_zone.gcp_sub_zone[0].name : ""
 }
 
 output "gcp_dns_zone_nameservers" {
-  value = module.dns-multicloud.gcp_dns_zone_nameservers[0]
+  value = var.create_gcp_dns_zone ? google_dns_managed_zone.gcp_sub_zone[0].name_servers : []
 }
