a9d6eac564a27b304568ec5b82c1d6335dfb390f
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-11-17 12:51:52 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 module "dns-multicloud" {
-  source              = "git::https://github.com/lhaig/terraform-dns-multicloud.git?ref=v0.3.0"
+ source              = "git::https://github.com/lhaig/terraform-dns-multicloud.git?ref=0.2.2"
   owner	              = var.owner
   namespace		         = var.namespace
   created-by		       = var.created-by
+  aws_region	         = var.aws_region
   gcp_project	          = var.gcp_project
+  gcp_region		        = var.gcp_region
   azure_location		    = var.azure_location
   create_gcp_dns_zone		= var.create_gcp_dns_zone
   create_aws_dns_zone		= var.create_aws_dns_zone
